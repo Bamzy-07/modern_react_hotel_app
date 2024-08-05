a9ffdb21d9dd53e7a6b89a95ba5397cbad46ff62
Bamiebo Obed <kobbiebamzy@gmail.com>
2024-08-05 07:16:59 +0000

    retrial

--- src/containers/Hero.jsx
+++ src/containers/Hero.jsx
@@ -19,7 +19,10 @@ const Hero = () => {
         <div className='flex-1 flex flex-col items-center'>
           <p className='font-poppins text-[20px] pb-6 text-black font-normal'>Since 1990</p>
           <h1 className='text-[35px] sm:text-[48px] text-black uppercase text-center font-serif font-semibold tracking-wider'>Stunning Switzerland <span className='text-gradient'>Landmark</span></h1>
-          <p className={`${styles.paragraph} text-justify pt-16 sm:pt-20`}>Lorem </p>
+          <p className={`${styles.paragraph} text-justify pt-16 sm:pt-20`}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla incidunt accusantium quo temporibus! Eos eaque obcaecati reiciendis dolorem illo, 
+            laudantium tempora consequuntur cupiditate sequi? Fugiat, asperiores! Exercitationem libero impedit atque. Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Dolorem dolores velit maxime dignissimos delectus culpa id similique quisquam praesentium odit! Laborum fugit excepturi voluptatum velit, 
+            illum quibusdam error reiciendis nisi!</p>
             <button className='font-serif font-semibold uppercase px-6 py-4 mt-8 text-[16px] text-white bg-primary border-2 hover:text-primary hover:bg-transparent hover:border-primary hover:scale-110 transition ease-in duration-500'>Read More</button>
         </div>
       </div>
